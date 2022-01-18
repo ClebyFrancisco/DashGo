4d3f00319e84c620fdcef47e9b4e523ae4af6e6c
Cleby Francisco <clebysilva5@gmail.com>
2022-01-17 21:08:49 -0300

    Configuring React Query and User Listing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/dist/shared/lib/router/router'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
 import { theme } from '../styles/theme';
@@ -9,14 +10,17 @@ if(process.env.NODE_ENV == 'development'){
   makeServer();
 }
 
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
